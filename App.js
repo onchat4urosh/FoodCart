f9c7bb9f089b01c5a0894d43a21a12f128f7cd56
onchat4urosh <roshanbaig04@gmail.com>
2020-06-03 14:16:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import AppStackNavigator from './src/AppStackNavigator'
+
+export default class App extends Component {
+  render() {
+    return (
+      <AppStackNavigator />
+    );
+  }
+}
